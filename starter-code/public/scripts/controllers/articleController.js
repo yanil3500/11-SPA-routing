50d4e5ac4ea01b041ea9a56c2273a272692380aa
Kaylee Alvarado <kaymay1000@gmail.com>
2017-02-21 09:33:46 -0800

    Lab 11 complete\!

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -2,7 +2,7 @@
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
 
